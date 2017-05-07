78317c963ce9b0cfe6ea9e32e88ef6d0cf64b0b8
Francesco <francesco.gorini2000@yahoo.com>
2017-05-07 20:43:19 +1200

    updated code

--- camera.cpp
+++ camera.cpp
@@ -1,51 +1,58 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <random>
+#include <string> 
 #include "E101.h"
-int main (){
-	init();
-	int counter = 0; //debugging
-	 while(true){
-	 	checkPath();
-	 	sleep1(1/30);
-	 	counter ++; //debugging
-	 	if(counter == 30){
-	 		break;
-	 	}
-	 }
-	return 0;
-}
-int checkPath(){
-	take_picture(); //takes picture
-	int centered = checkPicture();
-	if(centered == 0){// if off trajectory
-		adjustTrajectory(); //adjust it
-	}
-	return 0;
-}
-int checkPicture(){//this function returns 0 if off trajectory and 1 if on trajectory
-	//Part 1
-	//Save image to array
-	int[] pictureArray_left = new int[21600];
-	int[] pictureArray_right = new int[21600];
+
+int checkPicture(){//this function returns which direction (if any) to turn the robot and by how many degrees
+	int pictureSum_left = 0;
+	int pictureSum_right = 0;
 	int rows = 240;
 	int columns = 360;
-	for(int i=0;i<columns;i+2){//image is 360x240
-		for(int j=0;j<rows;j+=2){// check every 2 pixels
-			int pixelColor = get_pixel(i,j,3);
-			int pos = j*i;
-			if(pos < 21600){ //left hand side of picture
-				pictureArray_left[pos] = pixelColor;
+	for(int i=1;i<columns+1;i+=4){//image is 360x240
+		for(int j=1;j<rows+1;j+=4){// check every 4 pixels
+			int pos [2] = {i, j};// X & Y
+			int random_color =  1 + (rand() % (int)(255 - 1 + 1));
+			if(i < 180){
+				if(random_color > (255/2)){
+					pictureSum_left++; //1 is white
+				}
 			}
-			else{//right hand side of picture
-				pictureArray_right[pos] = pixelColor;
+			else{
+				if(random_color > (255/2)){
+					pictureSum_right++;
+				}
 			}
+			counter ++;
 		}
 	}
-	//Psudo code
-	//if pictureArray_left > pictureArray_right then road is to the left and should turn left, otherwise right etcc.
-	
+	int difference = pictureSum_left - pictureSum_right;
+	if(pictureSum_left > pictureSum_right){ 
+		return 1;
+	}
+	else{
+		return 0;
+	}
 }
-int adjustTrajectory(){
+int adjustTrajectory(int direction, int degrees){
 	//set_motor(0,255);
 	//et_motor(1,255);
 	// here we will adjust the trajectory using functions like 15 degrees oand 30 degrees etc..
+}
+int main (){
+	init();
+	int x = 0;
+	while(x < 40){
+		int white = checkPicture();
+		if(white){
+			printf("turn left");
+		}
+		else{
+			printf("turn right");
+		}
+		usleep(25000);
+		x++;
+	}
+	printf("finish");
+	return 0;
 }
