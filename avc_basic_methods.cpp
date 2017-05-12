57f04db6e224a73e7572e419baf61b918d573f6d
Jasper Kueppers <captchilko@gmail.com>
2017-05-12 14:03:42 +1200

    Basic program acts like roomba

--- avc_basic_methods.cpp
+++ avc_basic_methods.cpp
@@ -10,15 +10,17 @@ int turn_right(double turnTime);
 int forward();
 int ifWall();
 int slowReverse();
+int z = 0;
 
 int main(){
 	//This sets up the RPi hardware and ensures everything is working correctly
 	init();
 	printf("ADC Mean: %d /n", adc_mean());
-	turn_left(0.5);
-	turn_right(0.5);
-	forward();
-	ifWall();
+	while(z == 0){
+		forward();
+		ifWall();
+		//printf("ADC Mean: %d /n", adc_mean());
+	}
 	stop(0);
 }
 
@@ -44,9 +46,9 @@ int turn_left(double turnTime){
 	
 	set_motor(1,0);
 	sleep1(0,010000);
-	set_motor(2,127); //assuming that motor 2 is right motor
+	set_motor(2,255); //assuming that motor 2 is right motor
 	sleep1(0, turnTime * 1000000);
-	stop(0);
+	
 	
 return 0;}
 
@@ -56,22 +58,22 @@ int turn_right(double turnTime){
 	sleep1(0,010000);
 	set_motor(1,127); //assuming that motor 1 is left motor
 	sleep1(0, turnTime * 1000000);
-	stop(0);
+	
 	
 return 0;}
 
 int forward(){
 	
-	set_motor(1,-255);
+	set_motor(1,255);
 	sleep1(0,010000);//sleep may not be needed/wanted
 	set_motor(2,255);
-	sleep1(0,010000);
+	sleep1(1,000000);
 	
 return 0;}
 
 int slowReverse(){
 	
-	set_motor(1,100);
+	set_motor(1,-100);
 	sleep1(0,010000); //sleep may not be needed/wanted
 	set_motor(2,-100);
 	sleep1(0,010000);
@@ -80,8 +82,10 @@ return 0;}
 
 int ifWall(){
 	
-	if (adc_mean() > 300){
+	if (adc_mean() > 100){
 		slowReverse();
+		sleep1(1,200000);
+		turn_left(0.9999);
 	}
 	
 return 0;}
