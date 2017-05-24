291961d259f5179675eb107694611279d1e4eb5b
Jasper Kueppers <captchilko@gmail.com>
2017-05-24 15:53:57 +1200

    Update dMain.cpp

--- dMain.cpp
+++ dMain.cpp
@@ -158,8 +158,8 @@ int doQ3(){
 		sleep1(0,100000);
 	}  else { 
 		
-		v_left = initSpeed - pSignal;
-		v_right	= initSpeed + pSignal;
+		v_left = initSpeed + pSignal;
+		v_right	= initSpeed - pSignal;
 		set_motor(MRIGHT,v_right);
     		set_motor(MLEFT,v_left); 
 	}
